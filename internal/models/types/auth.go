a6129e1d447dcc15ab256e5d7a33cd66d53cdd11
sstent <stuart.stent@gmail.com>
2025-09-22 05:40:43 -0700

    changed go mod name

--- internal/models/types/auth.go
+++ internal/models/types/auth.go
@@ -2,6 +2,11 @@ package types
 
 import "time"
 
+// TokenRefresher is an interface for refreshing a token.
+type TokenRefresher interface {
+	RefreshSession() error
+}
+
 // OAuthConsumer represents OAuth consumer credentials
 type OAuthConsumer struct {
 	ConsumerKey    string `json:"consumer_key"`
@@ -25,4 +30,18 @@ type OAuth2Token struct {
 	Scope        string    `json:"scope"`
 	CreatedAt    time.Time // Used for expiration tracking
 	ExpiresAt    time.Time // Computed expiration time
+}
+
+// Expired checks if token is expired
+func (t *OAuth2Token) Expired() bool {
+	return time.Now().After(t.ExpiresAt)
+}
+
+// RefreshIfNeeded refreshes token if expired
+func (t *OAuth2Token) RefreshIfNeeded(client TokenRefresher) error {
+	if !t.Expired() {
+		return nil
+	}
+
+	return client.RefreshSession()
 }
